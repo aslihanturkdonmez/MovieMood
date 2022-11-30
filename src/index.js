22f152ee1d1752c1aa7e177619c90c732fda4a43
aslihanturkdonmez <asliturkdonmez@hotmail.com>
2022-11-30 18:02:59 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react';
+import Navigator from './Navigator';
+
+const App = () => {
+  return (
+    <Navigator />
+  )
+}
+
+export default App;
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home;
