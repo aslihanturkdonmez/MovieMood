17f5627b4b278adec8f4da12768fbb7c686bbdef
aslihanturkdonmez <asliturkdonmez@hotmail.com>
2022-12-02 01:48:23 +0300

    movie card design

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,12 +1,25 @@
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import React from 'react';
 import styles from './MovieCard.style';
 import Text from '../Text';
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({ movie }) => {
+
   return (
-    <View style={{}}>
-      <Text>{movie.Title}</Text>
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Image
+          source={{ uri: movie.Poster }}
+          style={styles.img}
+        />
+        <View style={styles.infoContainer}>
+          <Text style={styles.title}>{movie.Title}</Text>
+          <View>
+            <Text style={styles.year}>Year: {movie.Year}</Text>
+            <Text style={styles.type}>Type: {movie.Type}</Text>
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
--- src/components/MovieCard/MovieCard.style.js
+++ src/components/MovieCard/MovieCard.style.js
@@ -1,5 +1,40 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-
+    container:{
+        marginHorizontal:10, 
+        borderWidth:1, 
+        borderRadius:8, 
+        borderColor:'#cecece',
+    },
+    innerContainer:{
+        flexDirection:'row',
+    },
+    img:{
+        width:85, 
+        height:115, 
+        resizeMode:'cover', 
+        borderRadius:8,
+    },
+    infoContainer:{
+        flex:1, 
+        paddingBottom:5,
+        justifyContent:'space-around',
+    },
+    title:{
+        fontWeight:'bold', 
+        paddingHorizontal:12, 
+        fontSize:17, 
+        marginVertical:5,
+    },
+    year:{ 
+        paddingHorizontal:12, 
+        fontSize:14, 
+        marginTop:5,
+    },
+    type:{
+        paddingHorizontal:12, 
+        fontSize:14, 
+        marginTop:5,
+    }
 });
