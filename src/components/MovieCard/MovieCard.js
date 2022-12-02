e986bf3e7484fac80c7fee1e0f4f279d68f6140b
aslihanturkdonmez <asliturkdonmez@hotmail.com>
2022-12-02 03:19:57 +0300

    use progressive img instead of rn img

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,16 +1,19 @@
-import { View, Image } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
 import styles from './MovieCard.style';
 import Text from '../Text';
+import ProgressiveImage from '../ProgressiveImage';
 
 const MovieCard = ({ movie }) => {
 
   return (
     <View style={styles.container}>
       <View style={styles.innerContainer}>
-        <Image
+        <ProgressiveImage
           source={{ uri: movie.Poster }}
           style={styles.img}
+          resizeMode={'cover'}
+          containerStyle={styles.img}
         />
         <View style={styles.infoContainer}>
           <Text style={styles.title}>{movie.Title}</Text>
