083f437f57f9fbf3c9c4827a1e809c77e12cde93
aslihanturkdonmez <asliturkdonmez@hotmail.com>
2022-12-03 05:38:43 +0300

    edit ios statusbar color

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,11 +1,15 @@
-import { View, Text } from 'react-native'
+import { View,  StatusBar, Platform} from 'react-native'
 import React from 'react'
 import styles from './Container.style';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Container = ({children, style}) => {
   return (
     <View style={[styles.container, style]}>
+      {
+        Platform.OS === 'ios' ? 
+        <StatusBar barStyle={'light-content'} />
+        : null
+      }
         {children}
     </View>
   )
