f1f5bad33eefd7b20874aad6416526d35d5599d1
aslihanturkdonmez <asliturkdonmez@hotmail.com>
2022-12-03 03:22:10 +0300

    create colors file

---
+++ src/resources/colors/colors.js
@@ -0,0 +1,16 @@
+export default {
+    background:'#080c24',
+    title:'#eaedfb',
+    backIcon:'#eaedfb',
+    headerBorder:'rgba(255,255,255,0.1)',
+    searhBar:'#222c50',
+    text:'#fff',
+    searchBarText:'#4e597c',
+    searchBarIcon:'#bac5e5',
+    searchBarLine:'#0e1737',
+    movieDetailType:'#686f87',
+    movieDetailWriter:'#686f87',
+    movieDetailDesc:'#a7abbe',
+    infoBox:'#222c50',
+
+}
