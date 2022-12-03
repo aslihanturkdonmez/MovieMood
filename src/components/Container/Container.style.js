a05e132782f334bb290703641c0626d0ef9b03d0
aslihanturkdonmez <asliturkdonmez@hotmail.com>
2022-12-03 04:50:35 +0300

    add background color to container component

--- src/components/Container/Container.style.js
+++ src/components/Container/Container.style.js
@@ -1,7 +1,9 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../resources/colors/colors';
 
 export default StyleSheet.create({
     container:{
         flex:1,
+        backgroundColor:colors.background,
     }
 })
