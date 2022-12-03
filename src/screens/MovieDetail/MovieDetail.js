a4b5919e8de8050077b048d6efcc57525da3cb94
aslihanturkdonmez <asliturkdonmez@hotmail.com>
2022-12-03 05:41:28 +0300

    movie detail page

--- src/screens/MovieDetail/MovieDetail.js
+++ src/screens/MovieDetail/MovieDetail.js
@@ -1,13 +1,178 @@
-import { View } from 'react-native';
-import React from 'react';
-import { Container, Text } from '../../components';
+import { View, Dimensions, Pressable, ScrollView, Modal } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Container, Header, InfoBox, LoaderModal, ProgressiveImage, Text } from '../../components';
+import { fetchMovieDetail } from '../../services/Movie';
+import styles from './MovieDetail.style';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import FastImage from 'react-native-fast-image';
+
+const MovieDetail = ({ route, navigation }) => {
+    const { width, height } = Dimensions.get('window');
+    const { bottom } = useSafeAreaInsets();
+
+    const id = route.params.movieId;
+    const name = route.params.movieName;
+
+    const [movieDetail, setMovieDetail] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [readMore, setReadMore] = useState(false);
+    const [showImage, setShowImage] = useState(false);
 
 
-const MovieDetail = () => {
-  return (
-    <Container>
-    </Container>
-  )
+    useEffect(() => {
+        getMovieDetail();
+    }, []);
+
+    const getMovieDetail = async () => {
+        const res = await fetchMovieDetail({ id });
+        if (res.status === 200) {
+            setMovieDetail(res.data);
+        }
+        setLoading(false);
+    };
+
+    const onPressBack = () => {
+        navigation.goBack();
+    };
+
+    const toggleImgModalVisibility = () => {
+        setShowImage(!showImage);
+    };
+
+    const onPressImage = () => {
+        if (!readMore) {
+            setShowImage(true);
+        } else {
+            setReadMore(!readMore);
+        }
+    };
+
+    const onMomentumScrollBegin = (event) => {
+        const offsetY = event.nativeEvent.contentOffset.y;
+        if (offsetY > 0) setReadMore(true);
+    };
+
+    const onPressCollapse = () => {
+        setReadMore(false);
+    };
+
+    const InfoIconText = ({ icon, text }) => {
+        return (
+            <View style={styles.infoIconInnerContainer}>
+                <Icon
+                    name={icon}
+                    style={styles.infoIcon}
+                />
+                <Text style={styles.infoText} >{text}</Text>
+            </View>
+        )
+    };
+
+    const renderImageModal = () => {
+        return (
+            <Modal visible={showImage} transparent={true}>
+                <Pressable style={styles.imgModalBack} onPress={toggleImgModalVisibility}>
+                    <FastImage
+                        source={{ uri: movieDetail.Poster }}
+                        style={{ width: width, height }}
+                        resizeMode='contain'
+                    />
+                </Pressable>
+            </Modal>
+        )
+    };
+
+
+    return (
+        <Container>
+            <Header
+                header={name}
+                onPressBack={onPressBack}
+            />
+                { loading ?
+                    <LoaderModal visible={loading} />
+                    :
+                    <>
+                        <Pressable onPress={onPressImage} style={styles.imgContainer}>
+                            <ProgressiveImage
+                                source={{ uri: movieDetail.Poster }}
+                                containerStyle={styles.img({ width, height })}
+                                style={styles.img({ width, height })}
+                            />
+                        </Pressable>
+
+                        <View style={[styles.detailContainer, { flex: readMore ? 2.2 : 1.1 }]}
+                        >
+                            <ScrollView
+                                showsVerticalScrollIndicator={false}
+                                onMomentumScrollBegin={onMomentumScrollBegin}
+                                scrollEventThrottle={0}
+                                contentContainerStyle={[styles.scrollView, { paddingBottom: Math.max(bottom + 5, 10) }]}
+                            >
+                                <View>
+                                    <View>
+                                        <Text style={styles.type}>{movieDetail.Type.toUpperCase()}</Text>
+                                        <Text style={styles.title}>{movieDetail.Title}</Text>
+                                        <Text style={styles.genre}>{movieDetail.Genre}</Text>
+                                        <View style={styles.infoIconContainer}>
+                                            <InfoIconText
+                                                icon='movie-open'
+                                                text={movieDetail.Director}
+                                            />
+                                            <InfoIconText
+                                                icon='grease-pencil'
+                                                text={movieDetail.Writer}
+                                            />
+                                            <InfoIconText
+                                                icon='human-male-male'
+                                                text={movieDetail.Actors}
+                                            />
+                                            <InfoIconText
+                                                icon='trophy-award'
+                                                text={movieDetail.Awards}
+                                            />
+                                            <InfoIconText
+                                                icon='calendar'
+                                                text={movieDetail.Released}
+                                            />
+                                        </View>
+                                    </View>
+                                    <View style={styles.descContainer}>
+                                        <Text style={styles.desc}>{movieDetail.Plot}</Text>
+                                    </View>
+                                    <View style={styles.infoBoxContainer}>
+                                        <InfoBox
+                                            text={movieDetail.Year}
+                                        />
+                                        {/*<InfoBox
+                                text={movieDetail.Country}
+                            /> */}
+
+                                        <InfoBox
+                                            text={movieDetail.imdbVotes + ' Votes'}
+                                        />
+                                        <InfoBox
+                                            text={movieDetail.Runtime}
+                                        />
+
+                                    </View>
+                                </View>
+                                {
+                                    readMore &&
+                                    <View style={styles.collapseContainer}>
+                                        <Pressable onPress={onPressCollapse} style={styles.collapseInnerContainer}>
+                                            <Text>Collapse...</Text>
+                                        </Pressable>
+                                    </View>
+                                }
+                            </ScrollView>
+                        </View>
+                        {renderImageModal()}
+                    </>
+                }
+        </Container>
+    )
 }
 
 export default MovieDetail;
--- src/screens/MovieDetail/MovieDetail.style.js
+++ src/screens/MovieDetail/MovieDetail.style.js
@@ -1,7 +1,96 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../resources/colors/colors';
 
 export default StyleSheet.create({
     container:{
         
-    }
+    },
+    imgContainer:{ 
+        flex: 1.5, 
+        alignItems: 'center', 
+    },
+    img: ({width, height}) => ({
+        width:width-80,
+        height:height / 2,  
+        borderRadius:12, 
+    }),
+    detailContainer:{
+        paddingHorizontal:20, 
+        marginTop:-10,
+        marginTop:10,
+        paddingTop:10,
+        backgroundColor:colors.background,
+        borderRadius:20,
+        borderColor:colors.infoBox,
+        borderTopWidth:1,
+        borderEndWidth:1,
+        borderStartWidth:1,
+
+    },
+    type:{
+        fontSize:13, 
+        fontWeight:'bold',
+        color:colors.movieDetailType,
+        marginBottom:8,
+    },
+    title:{
+        fontWeight:'bold',
+        fontSize:22,
+    },
+    descContainer:{
+        marginVertical: 12,
+    },
+    desc:{
+        color:colors.movieDetailDesc,
+
+    },
+    infoText:{
+        color:colors.movieDetailWriter,
+        fontSize:13,
+        flex:1,
+    },
+    infoIcon:{
+        color:colors.movieDetailWriter,
+        fontSize:15,
+        marginRight:5,
+    },
+    infoIconContainer:{ 
+        marginTop: 12, 
+    },
+    infoIconInnerContainer:{
+        flexDirection:'row', 
+        alignItems:'flex-start',
+        marginBottom:6,
+    },
+    genre:{
+        fontSize:15,
+        color:colors.movieDetailType,
+        marginTop:1,
+    },
+    readMore:{
+        marginTop:3,
+        color:colors.movieDetailDesc,
+    },
+    imgModalBack:{
+        backgroundColor:colors.transparentGray,
+    },
+    scrollView:{
+        justifyContent: 'flex-start', 
+        flexGrow: 1, 
+        justifyContent: 'space-between',
+    },
+    infoBoxContainer:{ 
+        flexDirection: 'row', 
+        justifyContent: 'space-around', 
+        marginBottom: 12, 
+    },
+    collapseContainer:{ 
+        alignItems: 'flex-start', marginTop: 20, 
+    },
+    collapseInnerContainer:{ 
+        paddingHorizontal: 22, 
+        paddingVertical: 10, 
+        backgroundColor: '#0e153f', 
+        borderRadius:8, 
+    },
 })
