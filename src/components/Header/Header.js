3fefef94c951958edd2351926d655f6c32b1d70d
aslihanturkdonmez <asliturkdonmez@hotmail.com>
2022-12-03 03:23:10 +0300

    edit header style & add back button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,23 @@ import React from 'react';
 import Text from '../Text';
 import styles from './Header.style';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Header = ({ header, containerStyle, headerStyle }) => {
+const Header = ({ header, containerStyle, headerStyle, onPressBack }) => {
     return (
         <View style={[styles.container({ safeAreaInsets: useSafeAreaInsets() }), containerStyle]}>
+            {
+                onPressBack ? 
+                <Icon 
+                    name='arrow-left'
+                    style={styles.icon}
+                    onPress={onPressBack}
+                /> 
+                : 
+                <View style={styles.icon} />
+            }
             <Text style={[styles.header, headerStyle]}>{header}</Text>
+            <View style={styles.icon} />
         </View>
     )
 }
--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,14 +1,26 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../resources/colors/colors';
 
 export default StyleSheet.create({
     container: ({safeAreaInsets}) => ({
         flexDirection:'row',
-        justifyContent:'center',
         paddingTop: Math.max(safeAreaInsets.top + 5, 10),
         marginBottom:10,
+        justifyContent:'space-between',
+        paddingHorizontal:20,
+        alignItems:'center',
+        paddingBottom:10,
+        borderBottomWidth:1,
+        borderColor:colors.headerBorder,
     }),
     header:{
         fontSize:20,
         fontWeight:'bold',
-    }
+        color:colors.title,
+    },
+    icon:{
+        width:20, 
+        fontSize:20,
+        color:colors.backIcon
+    },
 })
